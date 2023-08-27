74b6f1a5c2f621785028ca88007d0aa2ad57e62e
Lucas Carneiro <142678881+Carneiro9975@users.noreply.github.com>
2023-08-27 04:30:58 -0300

    teste

---
+++ Teste.py
@@ -0,0 +1,40 @@
+nome = input('Qual o seu nome?')
+salario = float(input('Quanto é o seu salario?'))
+anos = int(input('Quantos anos você trabalha na empresa?'))
+if anos < 3:
+    print('Seu aumento é',salario*0.03 + salario)
+elif anos >= 3 < 9:
+    print('Seu aumento será',salario*0.125 + salario)
+elif anos >= 10:
+    print('Seu aumento será',salario*0.20 + salario)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              
+
+
+
+
+
+
+
+
+
+
+
